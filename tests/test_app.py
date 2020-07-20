5cd6da080e62996dad99355920c69a33c1747d6a
Mikhail Migushov <me@chuvash.pw>
2020-07-20 12:12:45 +0300

    Tests for file upload

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,5 @@
+import hashlib
+
 import aiohttp.web
 
 
@@ -5,6 +7,23 @@ def test_create_app(test_app):
     assert isinstance(test_app, aiohttp.web.Application)
 
 
-async def test_upload_file(app_client):
-    res = await app_client.post('/files', data={'file': open('tests/conftest.py', 'rb')})
-    assert res.status == 500
+async def test_uploaded_file_hash(app_client):
+    file = open('tests/conftest.py', 'rb')
+    f_hash = hashlib.sha256()
+    f_hash.update(file.read())
+
+    res = await app_client.post('/files', data={'file': file})
+    assert res.status == 200
+
+    data = await res.json()
+    assert data.get('hash') == f_hash.hexdigest()
+
+
+async def test_upload_file_bad_content_type(app_client):
+    res = await app_client.post('/files', json={'this': 'should not work'})
+    assert res.status == 400
+
+
+async def test_upload_empty_file(app_client):
+    res = await app_client.post('/files', data={'file': b''})
+    assert res.status == 400
