1b84b8f65ec620cd23a09c9f0cc3740de826e462
Mikhail Migushov <me@chuvash.pw>
2020-07-20 18:26:35 +0300

    Tests for file download

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import random
+
 import pytest
 
 import file_server
@@ -11,3 +13,11 @@ def test_app():
 @pytest.fixture
 def app_client(test_app, loop, aiohttp_client):
     return loop.run_until_complete(aiohttp_client(test_app))
+
+
+@pytest.fixture
+async def uploaded_file(app_client):
+    contents = bytes([random.getrandbits(8) for _ in range(0, 100)])    # 100 random bytes
+    res = await app_client.post('/files/', data={'file': contents})
+    data = await res.json()
+    return {'hash': data['hash'], 'contents': contents}
--- tests/test_app.py
+++ tests/test_app.py
@@ -27,3 +27,20 @@ async def test_upload_file_bad_content_type(app_client):
 async def test_upload_empty_file(app_client):
     res = await app_client.post('/files/', data={'file': b''})
     assert res.status == 400
+
+
+async def test_get_file(app_client, uploaded_file):
+    res = await app_client.get(f'/files/{uploaded_file["hash"]}')
+    assert res.status == 200
+
+    got_file = await res.read()
+
+    f_hash = hashlib.sha256()
+    f_hash.update(got_file)
+
+    assert f_hash.hexdigest() == uploaded_file['hash']
+
+
+async def test_get_nonexitstent_file(app_client):
+    res = await app_client.get('/files/1234567890987654321521bc6955deadbeef31e11aaaaaaaaaaaaaaaaaaaaa3f')
+    assert res.status == 404
